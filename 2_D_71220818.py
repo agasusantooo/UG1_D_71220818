37c830b5951e09c5a3d69b08bbcbaa941aea59a9
agasusantooo <117900754+agasusantooo@users.noreply.github.com>
2023-02-08 19:22:49 +0700

    Add files via upload

--- 2_D_71220818.py
+++ 2_D_71220818.py
@@ -2,6 +2,7 @@ jumlahkata=0
 kalimat=input("Kalimat yang ingin diteliti :")
 kalimat=kalimat.lower()
 katayangdicari=input("Kata yang dicari:")
+katayangdicari=katayangdicari.lower()
 kalimat_list=kalimat.split()
 
 for kata in kalimat_list:
