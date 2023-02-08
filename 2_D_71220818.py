abecb3d3555bcc6abd5541dfb5404c08a613a571
agasusantooo <117900754+agasusantooo@users.noreply.github.com>
2023-02-08 19:21:30 +0700

    Add files via upload

---
+++ 2_D_71220818.py
@@ -0,0 +1,11 @@
+jumlahkata=0
+kalimat=input("Kalimat yang ingin diteliti :")
+kalimat=kalimat.lower()
+katayangdicari=input("Kata yang dicari:")
+kalimat_list=kalimat.split()
+
+for kata in kalimat_list:
+    if katayangdicari == kata:
+        jumlahkata += 1
+        
+print(jumlahkata)
